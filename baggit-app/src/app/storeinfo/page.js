28ba143fe73d09681c61d00c587c8b4ca1083b70
peposlawa <peposlawa@gmail.com>
2024-09-26 18:38:15 +0100

    BAGGIT APP v1.0

--- baggit-app/src/app/storeinfo/page.js
+++ baggit-app/src/app/storeinfo/page.js
@@ -1,12 +1,17 @@
-'use client'
+"use client";
 
-import { useState, React } from 'react';
-import styles from './page.module.css';
-import Nav from '../components/Navigation/Nav';
-import Image from 'next/image';
+import { useState, React } from "react";
+import styles from "./page.module.css";
+import Nav from "../components/Navigation/Nav";
+import Image from "next/image";
 // import supabase from '@/config/supabaseClient';
 
 const App = () => {
+  const [showTopReviews, setShowTopReviews] = useState(true);
+
+  const toggleReviews = () => {
+    setShowTopReviews(!showTopReviews);
+  };
 
   // const [reviews, setReviews] = useState([]);
 
@@ -31,10 +36,10 @@ const App = () => {
   //     setReviews(formattedReviews);
   //     console.log(formattedReviews);
   //   }
-    
+
   //   //   const ratingsArray = formattedReviews.map(item => item.rating)
   //   //   console.log('array of ratings for this review: ', ratingsArray);
-      
+
   //   //   function findAverage (arr) {
   //   //     let sumofArr = 0;
   //   //     for (let i = 0; i < arr.length; i++) {
@@ -48,103 +53,241 @@ const App = () => {
   //   // };
   //   // console.log(average);
 
-
   //   fetchSingleShopReview(2);
   //   const averageRating = 4;
+
   return (
     <>
-    <div className={styles.container}>
-      {/* Location Description */}
-      <div className={styles.locationDescription}>
-        <h2>Next</h2>
-        <div className={styles.details}>
-          <p><strong>Opening times:</strong> Mon-Sun: 8:00 - 22:00</p>
-          <p><strong>Features:</strong> Large and small bags allowed</p>
-          <p>Adress will be show after booking</p>
+      <div className={styles.container}>
+        {/* Location Description */}
+        <div className={styles.locationDescription}>
+          <h2>Hobbs Birmingham</h2>
+          <div className={styles.details}>
+            <p>
+              <strong>Opening times:</strong> Mon-Fri 9:00 - 20:00
+            </p>
+            <p>
+              <strong>Features:</strong> Large and small bags allowed
+            </p>
+            <div className={styles.protection}>
+              <Image
+                src="/shield.png"
+                alt="protection"
+                width={17}
+                height={17}
+              />
+              <p>Protection up to £5,000</p>
+            </div>
+          </div>
+          <div className={styles.reviews}>Reviews</div>
         </div>
-      </div>
-      <div className={styles.protection}>
-      <Image src="/shield.png" alt="protection" width={15} height={15} />
-      <p>Protection up to £5,000</p>
-      </div>
-      <div className={styles.rating}>
-      <h2>4.0</h2>
-      <div>
-        <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-        <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-        <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-        <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-        <Image src="/emptystar.png" alt="empty star" width={15} height={15} />
-        <p>3 reviews</p>
-      </div>
-      </div>
-      <div className={styles.smallButtons}>
-      <button className={styles.buttonsButton} >Top Reviews</button>
-      <button className={styles.buttonsButton} >Most Recent</button>
-      </div>
-      <div>
-      <h3>Hannah</h3>
-      <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-      <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-      <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-      <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-      <Image src="/emptystar.png" alt="empty star" width={15} height={15} />
-      
-      </div>
-      <p>&quot;Efficient service, but the storage space was cramped. My bags were safe, though, so no major complaints.&quot;</p>
-      <div>
-      <h3>David</h3>
-      <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-      <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-      <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-      <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-      <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-      </div>
-      <p>&quot;Wonderful app!&quot;</p>
-      <div>
-      <h3>Mike</h3>
-      <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-      <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-      <Image src="/completedstar.png" alt="full star" width={15} height={15} />
-      <Image src="/emptystar.png" alt="full star" width={15} height={15} />
-      <Image src="/emptystar.png" alt="full star" width={15} height={15} />
-      </div>
-      <p>&quot;This shop had great bag security but the shopkeeper was very impolite. I would recommend walking further to a different store.&quot;</p>
-      {/* About Section */}
-      <div className={styles.aboutApp}>
-        <h3>About Bag Storage App</h3>
-        <p>How it works: Simply choose a location, select a time, and pay securely. Read more about our <a href='/terms'> Terms and Conditions </a> </p><br></br>
-      </div>
-      {/* Select Booking Duration */}
-      <div className={styles.bookingDuration}>
-        <h3>Select Booking Duration</h3>
-        <div className={styles.bookingDurationContainer}>
-          <div className={styles.bookingDurationOption}>1 hour</div>
-          <div className={styles.bookingDurationOption}>3 hours</div>
-          <div className={styles.bookingDurationOption}>6 hours</div>
-          <div className={styles.bookingDurationOption}>12 hours</div>
-        </div>
-        <p className={styles.bookingText}>Choose the time duration for storing your bag</p>
-      </div>
-      {/* Buttons for Add Bag, Settings, and Pay Now */}
-      <div className={styles.buttons}>
-        <div className={styles.buttonsButton}>Add New Bag</div>
-        <div className={styles.buttonsButton}>Settings</div>
-        <div className={styles.buttonsPayNow}>Pay Now</div>
-      </div>
-      {/* User Section
-      <div className={styles.userSection}>
-        <div className={styles.userInfo}>
-          <Image src="/baggitLogoRed.svg" alt="LogoBaggIt" width={40} height={40} />
+
+        <div className={styles.rating}>
+          <h2>4.0</h2>
           <div>
-            <h4>John Doe</h4>
-            <p>Welcome back!</p>
+            <Image
+              src="/completedstar.png"
+              alt="full star"
+              width={15}
+              height={15}
+            />
+            <Image
+              src="/completedstar.png"
+              alt="full star"
+              width={15}
+              height={15}
+            />
+            <Image
+              src="/completedstar.png"
+              alt="full star"
+              width={15}
+              height={15}
+            />
+            <Image
+              src="/completedstar.png"
+              alt="full star"
+              width={15}
+              height={15}
+            />
+            <Image
+              src="/emptystar.png"
+              alt="empty star"
+              width={15}
+              height={15}
+            />
+            <p>3 reviews</p>
           </div>
         </div>
-      </div> */}
-    </div>
-    <Nav />
-  </>
+
+        <div className={styles.reviewToggleContainer}>
+          <button className={styles.reviewToggleButton} onClick={toggleReviews}>
+            {showTopReviews ? "Show Recent Reviews 🡇" : "Show Top Reviews 🡇"}
+          </button>
+
+          {/* Conditional rendering of reviews based on state */}
+          {showTopReviews ? (
+            // Render top reviews here
+            <>
+              <div className={styles.reviewContainer}>
+                <h2>Hannah, London</h2>
+                <div className={styles.stars}>
+                  <Image
+                    src="/completedstar.png"
+                    alt="full star"
+                    width={15}
+                    height={15}
+                  />
+                  <Image
+                    src="/completedstar.png"
+                    alt="full star"
+                    width={15}
+                    height={15}
+                  />
+                  <Image
+                    src="/completedstar.png"
+                    alt="full star"
+                    width={15}
+                    height={15}
+                  />
+                  <Image
+                    src="/completedstar.png"
+                    alt="full star"
+                    width={15}
+                    height={15}
+                  />
+                  <Image
+                    src="/emptystar.png"
+                    alt="empty star"
+                    width={15}
+                    height={15}
+                  />
+                </div>
+              </div>
+              <p className={styles.reviewText}>
+                Efficient service, but the storage space was so cramped. My bags
+                were safe, though, so no major complaints.
+              </p>
+
+              <div className={styles.reviewContainer}>
+                <h2>Mandy, Glasgow</h2>
+                <div className={styles.stars}>
+                  <Image
+                    src="/completedstar.png"
+                    alt="full star"
+                    width={15}
+                    height={15}
+                  />
+                  <Image
+                    src="/completedstar.png"
+                    alt="full star"
+                    width={15}
+                    height={15}
+                  />
+                  <Image
+                    src="/completedstar.png"
+                    alt="full star"
+                    width={15}
+                    height={15}
+                  />
+                  <Image
+                    src="/completedstar.png"
+                    alt="full star"
+                    width={15}
+                    height={15}
+                  />
+                  <Image
+                    src="/completedstar.png"
+                    alt="empty star"
+                    width={15}
+                    height={15}
+                  />
+                </div>
+              </div>
+              <p className={styles.reviewText}>
+                The staff were very friendly and helpful. The storage space was
+                clean and secure. I loved the app and the process!
+              </p>
+            </>
+          ) : (
+            <>
+              <div className={styles.reviewContainer}>
+                <h2>Mike, Birmingham</h2>
+                <div className={styles.stars}>
+                  <Image
+                    src="/completedstar.png"
+                    alt="full star"
+                    width={15}
+                    height={15}
+                  />
+                  <Image
+                    src="/completedstar.png"
+                    alt="full star"
+                    width={15}
+                    height={15}
+                  />
+                  <Image
+                    src="/completedstar.png"
+                    alt="full star"
+                    width={15}
+                    height={15}
+                  />
+                  <Image
+                    src="/emptystar.png"
+                    alt="full star"
+                    width={15}
+                    height={15}
+                  />
+                  <Image
+                    src="/emptystar.png"
+                    alt="full star"
+                    width={15}
+                    height={15}
+                  />
+                </div>
+              </div>
+              <p className={styles.reviewText}>
+                This shop had great bag security but the shopkeeper was very
+                impolite. I honestly would recommend walking further to a
+                different store.
+              </p>
+              {/* ... other recent reviews ... */}
+            </>
+          )}
+        </div>
+
+        {/* About Section */}
+        <div className={styles.aboutApp}>
+          <h3>About Bag Storage App</h3>
+          <p>
+            How it works: Simply choose a location, select a time, and pay
+            securely. Read more about it in our{" "}
+            <a href="/terms">Terms and Conditions</a>.
+          </p>
+        </div>
+
+        {/* Select Booking Duration */}
+        <div className={styles.bookingDuration}>
+          <h3>Select Booking Duration</h3>
+          <p className={styles.bookingText}>
+            Choose the time duration for storing your bag
+          </p>
+
+          <div className={styles.bookingDurationContainer}>
+            <div className={styles.bookingDurationOption}>1 hour</div>
+            <div className={styles.bookingDurationOption}>3 hours</div>
+            <div className={styles.bookingDurationOption}>6 hours</div>
+            <div className={styles.bookingDurationOption}>12 hours</div>
+          </div>
+        </div>
+        {/* Buttons for Add Bag, Settings, and Pay Now */}
+        <div className={styles.buttons}>
+          <div className={styles.addBagButton}>Add New Bag</div>
+          <div className={styles.settingsButton}>Settings</div>
+          <div className={styles.payNowButton}>Pay Now</div>
+        </div>
+      </div>
+      <Nav />
+    </>
   );
 };
 
